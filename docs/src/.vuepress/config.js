b7746373cfbeded099e3d9f36f0d16e414be5dae
Alexander <sashafromlibertalia@gmail.com>
2022-06-19 22:06:52 +0300

    feat: added vuepress

---
+++ docs/src/.vuepress/config.js
@@ -0,0 +1,43 @@
+const { description } = require('../../package')
+
+module.exports = {
+  title: 'Базы данных. Полный курс',
+  description: description,
+  theme: 'yuu',
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+  base: '/itmo-db-course/',
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'Весь курс',
+        link: '/exam/',
+      },
+      {
+        text: 'GitHub',
+        link: 'https://github.com/sashafromlibertalia/itmo-db-course'
+      }
+    ],
+    sidebar: {
+      '/exam/': [
+        {
+          title: 'Полный курс',
+          collapsable: false,
+          children: Array(40).fill().map((_, i) => `Question_${i + 1}.md`)
+        }
+      ],
+    }
+  },
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
---
+++ docs/src/index.md
@@ -0,0 +1,8 @@
+---
+home: true
+heroImage: http://isu.ifmo.ru/pls/apex/f?p=2143:0::AVATAR:NO::T_UID:114568
+tagline: 
+actionText: Изучить →
+actionLink: /exam/
+footer: Сделано в содружестве с Липой и Искандером ❤️
+---
